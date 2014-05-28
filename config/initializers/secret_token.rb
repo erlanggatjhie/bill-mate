a42b22f83a40cf1b903d230fd4fdbcd2c225a200
root <erlanggatjhie@crunchbang>
2014-05-28 22:56:04 +1000

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BillMate::Application.config.secret_key_base = '7876485f3b72df9865e2dff9e3f8f49700943d444ddd765c56e7cfbdf56618b957f543efdca6feb0c82fc21a85c10680c564f19db3e54c8a4e076297c60de711'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BillMate::Application.config.session_store :cookie_store, key: '_bill-mate_session'
