3abfe32f843c0fc2b675636282b0f61c72f50b05
root <erlanggatjhie@crunchbang>
2014-05-29 00:17:03 +1000

    Deleted redundant files

--- spec/models/tenant_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe Tenant do
-	it { should respond_to(:first_name) }
-	it { should respond_to(:last_name) }
-	it { should respond_to(:email_address) }
-	it { should respond_to(:password) }
-	it { should respond_to(:salt) }
-end
